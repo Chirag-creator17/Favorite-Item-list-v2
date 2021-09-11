979f6007037b0a10ad0042fa1d9b875fe8fba566
Chirag Goel <76050897+Chirag-creator17@users.noreply.github.com>
2021-09-11 16:46:46 +0530

    Update User.js

--- routes/User.js
+++ routes/User.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 const userRouter = express.Router();
 const passport = require('passport');
 const passportConfig = require('../passport');
@@ -99,15 +99,6 @@ userRouter.get('/todos1',passport.authenticate('jwt',{session : false}),(req,res
             res.status(200).json({todos : document.todos1, authenticated : true});
         }
     });
-});
-
-
-userRouter.get('/admin',passport.authenticate('jwt',{session : false}),(req,res)=>{
-    if(req.user.role === 'admin'){
-        res.status(200).json({message : {msgBody : 'You are an admin', msgError : false}});
-    }
-    else
-        res.status(403).json({message : {msgBody : "You're not an admin,go away", msgError : true}});
 });
 
 userRouter.get('/authenticated',passport.authenticate('jwt',{session : false}),(req,res)=>{
