0b2a057b074181c66d5488dd02ef2d027782b6a5
Chirag Goel <76050897+Chirag-creator17@users.noreply.github.com>
2021-09-11 16:49:24 +0530

    Update Home.js

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,49 +1,24 @@
 import React from "react";
-import { Table, thread, th, tr, tbody, td } from "react-bootstrap";
-
+import { ListGroup } from "react-bootstrap";
+const friuts = ["Apple", "Banana", "Strawberries", "Mango", "Grapes"];
+const vegetables = ["Potato", "Tomato", "Onion", "Carrot", "Cabbage"];
 const Home = () => (
   <div>
-    <h1 style={{ paddingTop: 65 }}>Home Page</h1>
-    {/* <h1 ClassName="headers">Home Page</h1> */}
-    <br/>
-    <h2><u>Top 5 favourite Friuts and Vegetables</u></h2>
-    <br/>
-    <Table responsive borderless="true" size="sm" className="tables">
-      <thead>
-        <tr>
-          <th></th>
-          <th>Fruits</th>
-          <th>Vegetables</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Banana</td>
-          <td>Potatoes</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Apples</td>
-          <td>Tomatoes</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>Strawberries</td>
-          <td>Onions</td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>Grapes</td>
-          <td>Carrots</td>
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>Oranges</td>
-          <td>Lettuce </td>
-        </tr>
-      </tbody>
-    </Table>
+    <h1 className="head">Home Page</h1>
+    <ListGroup variant="flush" className="list">
+      <h3>Top 5 favorite fruits</h3>
+      <ul>
+        {friuts.map((friut, index) => (
+          <li key={index}>{friut}</li>
+        ))}
+      </ul>
+      <h3>Top 5 favorite vegetables</h3>
+      <ul>
+        {vegetables.map((vegetable, index) => (
+          <li key={index}>{vegetable}</li>
+        ))}
+      </ul>
+    </ListGroup>
   </div>
 );
 
